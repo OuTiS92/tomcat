d0e684084a20a0045c3f3666a72a42651d00dbf0
OuTiS <33751897+OuTiS92@users.noreply.github.com>
2023-09-28 15:20:32 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,54 +29,27 @@ sleep 2
 clear
 
 # and more user and password ...
-echo -n "<role rolename="manager-gui" />
-<user username="manager" password="manager" roles="manager-gui" />
-<role rolename="admin-gui" />
-<user username="admin" password="admin" roles="manager-gui,admin-gui" />" >> /opt/tomcat/conf/tomcat-users.xml 
-
+#echo -n "<role rolename="manager-gui" />
+#<user username="manager" password="manager" roles="manager-gui" />
+#<role rolename="admin-gui" />
+#<user username="admin" password="admin" roles="manager-gui,admin-gui" />" >> /opt/tomcat/conf/tomcat-users.xml 
+# new version for insert 
+cat  tomcat-users.xml >> /opt/tomcat/conf/tomcat-users.xml
 
 
 # and more services tomcat
 
-echo -n "{<?xml version="1.0" encoding="UTF-8"?>
-<Context antiResourceLocking="false" privileged="true" >
-  <CookieProcessor className="org.apache.tomcat.util.http.Rfc6265CookieProcessor"
-                   sameSiteCookies="strict" />
-<!--   <Valve className="org.apache.catalina.valves.RemoteAddrValve"
-         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" /> -->
-  <Manager sessionAttributeValueClassNameFilter="java\.lang\.(?:Boolean|Integer|Long|Number|String)|org\.apache\.catalina\.filters\.CsrfPreventionFilter\$LruCache(?:\$1)?|java\.util\.(?:Linked)?HashMap"/></Context>}" > /opt/tomcat/webapps/manager/META-INF/context.xml 
+#echo -n "{<?xml version="1.0" encoding="UTF-8"?>
+#<Context antiResourceLocking="false" privileged="true" >
+#  <CookieProcessor className="org.apache.tomcat.util.http.Rfc6265CookieProcessor"
+ #                  sameSiteCookies="strict" />
+#<!--   <Valve className="org.apache.catalina.valves.RemoteAddrValve"
+#         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" /> -->
+#  <Manager sessionAttributeValueClassNameFilter="java\.lang\.(?:Boolean|Integer|Long|Number|String)|org\.apache\.catalina\.filters\.CsrfPreventionFilter\$LruCache(?:\$1)?|java\.util\.(?:Linked)?HashMap"/></Context>}" > /opt/tomcat/webapps/manager/META-INF/context.xml 
 
-
-
-
-echo -n " 
-<?xml version="1.0" encoding="UTF-8"?>
-<!--
-  Licensed to the Apache Software Foundation (ASF) under one or more
-  contributor license agreements.  See the NOTICE file distributed with
-  this work for additional information regarding copyright ownership.
-  The ASF licenses this file to You under the Apache License, Version 2.0
-  (the "License"); you may not use this file except in compliance with
-  the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
--->
-<Context antiResourceLocking="false" privileged="true" >
-  <CookieProcessor className="org.apache.tomcat.util.http.Rfc6265CookieProcessor"
-                   sameSiteCookies="strict" />
-<!--  <Valve className="org.apache.catalina.valves.RemoteAddrValve"
-         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" /> -->
-  <Manager sessionAttributeValueClassNameFilter="java\.lang\.(?:Boolean|Integer|Long|Number|String)|org\.apache\.catalina\.filters\.CsrfPreventionFilter\$LruCache(?:\$1)?|java\.util\.(?:Linked)?HashMap"/>
-</Context>
-" > /opt/tomcat/webapps/host-manager/META-INF/context.xml 
-
-
+cat context.xml > /opt/tomcat/webapps/manager/META-INF/context.xml 
+cat context.xml >   /opt/tomcat/webapps/host-manager/META-INF/context.xml  
+ 
 # link for up and down webserver tomcat ...
 ln -s /opt/tomcat/bin/startup.sh /usr/local/bin/tomcatup
 ln -s /opt/tomcat/bin/shutdown.sh /usr/local/bin/tomcatdown
