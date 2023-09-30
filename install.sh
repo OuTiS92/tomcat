85f9c736e2f9bdfe503a4adff315d9c9a6c0420d
OuTiS <33751897+OuTiS92@users.noreply.github.com>
2023-09-30 22:21:12 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ clear
 # new version for insert 
 cat  tomcat-users.xml >> /opt/tomcat/conf/tomcat-users.xml
 
-# and more services tomcat
+# and more services tomcat ( dasorty ke nemikhaid az file estefade konid
 
 #echo -n "{<?xml version="1.0" encoding="UTF-8"?>
 #<Context antiResourceLocking="false" privileged="true" >
